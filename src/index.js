1adb1e1977c6c9d99de7792594bb4743f8e62060
Siddharth Negi <sidharthnegi43@gmail.com>
2021-11-14 20:13:01 +0530

    Created search component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SearchBar from "./components/searchBar";
+
 const API_KEY = "AIzaSyC3pONDOEZzW0HvWeNK1SwNCHuisJRs_rc";
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 ReactDOM.render(<App />, document.querySelector(".container"));
