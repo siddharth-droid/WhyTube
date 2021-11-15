12b3aa0d6bccdf2820676aa80ed3768af4294ca7
Siddharth Negi <sidharthnegi43@gmail.com>
2021-11-15 12:53:14 +0530

    Created videoList component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import YTSearch from "youtube-api-search";
+
 import SearchBar from "./components/searchBar";
+import VideoList from "./components/videoList";
 
 const API_KEY = "AIzaSyC3pONDOEZzW0HvWeNK1SwNCHuisJRs_rc";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 ReactDOM.render(<App />, document.querySelector(".container"));
